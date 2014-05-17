8baeb06b4ea32784fc77e8492289582a6ecc996e
rafgyr03 <arioc_herr01@hotmail.com>
2014-05-16 22:01:15 -0600

    fixed merge conflict

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,8 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("Hello GitHub");
-=======
-        System.out.println("Hello Github");
         System.out.println("Testing...");
->>>>>>> test
    }
 }
